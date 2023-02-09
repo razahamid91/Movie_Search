c833408c63c63bb80d567584b754caccac1b9d79
Hamid Raza <74106196+razahamid91@users.noreply.github.com>
2023-02-09 23:42:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { 
+   createBrowserRouter, RouterProvider 
+  } from "react-router-dom";
+import Home from "./components/Home";
+import SearchResult from "./components/SearchResult";
+import Movie from "./components/Movie";
+import { useEffect,useState } from "react";
+import Counter from "./components/Counter";
+import Actor from "./components/Actor";
+
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: '/search/:movie',
+      element:<SearchResult/>
+    },
+    {
+      path: '/movie/:movieId',
+      element:<Movie/>
+    },
+    {
+      path: '/counter',
+      element:<Counter/>
+    },
+    {
+      path: '/actor/:actorId',
+      element:<Actor/>
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
+      
     </div>
   );
 }
